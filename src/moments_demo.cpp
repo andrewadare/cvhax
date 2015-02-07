55235bd3c48e3df046d7bf44efd73b3a2475633d
Andrew Adare <andrewadare@gmail.com>
2015-02-06 17:52:04 -0700

    Added convex hull calculation

--- src/moments_demo.cpp
+++ src/moments_demo.cpp
@@ -9,6 +9,8 @@ using namespace cv;
 using namespace std;
 
 Mat src; Mat src_gray;
+
+// Pixel gradient intensity threshold
 int thresh = 100;
 int max_thresh = 255;
 RNG rng(12345);
@@ -17,19 +19,19 @@ void thresh_callback(int, void *);
 
 int main(int, char **argv)
 {
-  /// Load source image and convert it to gray
+  // Load source image and convert it to gray
   src = imread(argv[1], 1);
 
-  /// Convert image to gray and blur it
+  // Convert image to gray and blur it
   cvtColor(src, src_gray, COLOR_BGR2GRAY);
   blur(src_gray, src_gray, Size(3, 3));
 
-  /// Create Window
+  // Create Window
   const char *source_window = "Source";
   namedWindow(source_window, WINDOW_AUTOSIZE);
   imshow(source_window, src);
 
-  createTrackbar(" Canny thresh:", "Source", &thresh, max_thresh, thresh_callback);
+  createTrackbar("Canny threshold:", "Source", &thresh, max_thresh, thresh_callback);
   thresh_callback(0, 0);
 
   waitKey(0);
@@ -42,44 +44,66 @@ void thresh_callback(int, void *)
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
 
-  /// Detect edges using canny
+  // Detect edges using canny
   Canny(src_gray, canny_output, thresh, thresh * 2, 3);
-  /// Find contours
+
+  imshow("Result of Canny edge detection", canny_output);
+
+  // Find contours
   findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-  /// Get the moments
+  /// Find the convex hull object for each contour
+  vector<vector<Point> > hulls;
+  for ( size_t i = 0; i < contours.size(); i++ )
+  {
+    double area = contourArea(contours[i]);
+    if (area > 10)
+    {
+      vector<Point> hull;
+      convexHull(Mat(contours[i]), hull, false);
+      hulls.push_back(hull);
+    }
+  }
+
+  // Get the moments
   vector<Moments> mu(contours.size());
   for (size_t i = 0; i < contours.size(); i++)
   {
     mu[i] = moments(contours[i], false);
   }
 
-  ///  Get the mass centers:
+  //  Get the mass centers:
   vector<Point2f> mc(contours.size());
   for (size_t i = 0; i < contours.size(); i++)
   {
-    mc[i] = Point2f(static_cast<float>(mu[i].m10 / mu[i].m00) , static_cast<float>(mu[i].m01 / mu[i].m00));
+    mc[i] = Point2f(static_cast<float>(mu[i].m10 / mu[i].m00) ,
+                    static_cast<float>(mu[i].m01 / mu[i].m00));
   }
 
-  /// Draw contours
+  // Draw contours
   Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
   for (size_t i = 0; i < contours.size(); i++)
   {
-    Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+    Scalar color = Scalar(rng.uniform(0, 255),
+                          rng.uniform(0, 255),
+                          rng.uniform(0, 255));
     drawContours(drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point());
     circle(drawing, mc[i], 4, color, -1, 8, 0);
   }
 
-  /// Show in a window
+  // Show in a window
   namedWindow("Contours", WINDOW_AUTOSIZE);
   imshow("Contours", drawing);
 
-  /// Calculate the area with the moments 00 and compare with the result of the OpenCV function
+  // Calculate the area with the moments 00 and compare with the result of the OpenCV function
   printf("\t Info: Area and Contour Length \n");
   for (size_t i = 0; i < contours.size(); i++)
   {
-    printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", (int)i, mu[i].m00, contourArea(contours[i]), arcLength(contours[i], true));
-    Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+    printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n",
+           (int)i, mu[i].m00, contourArea(contours[i]), arcLength(contours[i], true));
+    Scalar color = Scalar(rng.uniform(0, 255),
+                          rng.uniform(0, 255),
+                          rng.uniform(0, 255));
     drawContours(drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point());
     circle(drawing, mc[i], 4, color, -1, 8, 0);
   }
