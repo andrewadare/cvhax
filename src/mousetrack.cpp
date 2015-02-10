fc88925807120d737e801de6b912f3dfb40e557e
Andrew Adare <andrewadare@gmail.com>
2015-02-10 11:09:37 -0700

    Rename variables, remove drawX(), more comments

--- src/mousetrack.cpp
+++ src/mousetrack.cpp
@@ -7,25 +7,24 @@
 using namespace cv;
 using namespace std;
 
-vector<Point> mousev, kalmanv;
 Point mouseCoords(-1, -1);
 Point lastMouse;
+vector<Point> mousePts, kfPts; // For drawing
 
 void onMouse(int event, int x, int y, int flags, void *param);
-void drawX(Mat &img, Point &center, Scalar color, int d);
 void drawPath(Mat &img, vector<Point> &points, Scalar color);
-void initKalmanFilter(KalmanFilter &KF,
+void initKalmanFilter(KalmanFilter &kf,
                       Point &xy,
                       Mat &transitionMatrix,
                       double processVariance,
                       double measurementVariance,
                       double postErrorVariance);
-void printKF(KalmanFilter &KF);
+void printKF(KalmanFilter &kf);
 
 int main(int argc, char *const argv[])
 {
   Mat img(500, 500, CV_8UC3);
-  KalmanFilter KF(4, 2, 0);
+  KalmanFilter kf(4, 2, 0);
   Mat measurement = Mat::zeros(2, 1, CV_32F);
   char code = (char) - 1;
   string windowName("Kalman Mouse Tracker");
@@ -42,31 +41,30 @@ int main(int argc, char *const argv[])
     }
     double procVar = 1e-4, measVar = 1e-3, errVar = 0.1;
     Mat tm = Mat::eye(4, 4, CV_32F);
-    initKalmanFilter(KF, mouseCoords, tm, procVar, measVar, errVar);
+    initKalmanFilter(kf, mouseCoords, tm, procVar, measVar, errVar);
 
-    mousev.clear();
-    kalmanv.clear();
+    mousePts.clear();
+    kfPts.clear();
 
     while (true)
     {
-      Mat prediction = KF.predict();
+      Mat prediction = kf.predict();
       Point predictPt(prediction.at<float>(0), prediction.at<float>(1));
 
+      // Cursor xy position
       measurement = (Mat_<float>(2, 1) << mouseCoords.x, mouseCoords.y);
 
-      Point measPt(measurement.at<float>(0), measurement.at<float>(1));
-      mousev.push_back(measPt);
-
-      Mat estimated = KF.correct(measurement);
-      Point statePt(estimated.at<float>(0), estimated.at<float>(1));
-      kalmanv.push_back(statePt);
+      // State vector estimated by Kalman Filter (x,y,vx,vy)
+      Mat kfstate = kf.correct(measurement);
 
       // Visualization
+      mousePts.push_back(Point(mouseCoords.x, mouseCoords.y));
+      kfPts.push_back(Point(kfstate.at<float>(0), kfstate.at<float>(1)));
       img = Scalar::all(0);
-      drawX(img, statePt, Scalar(255, 255, 255), 5);
-      drawX(img, measPt, Scalar(0, 0, 255), 5);
-      drawPath(img, mousev, Scalar(255, 255, 0));
-      drawPath(img, kalmanv, Scalar(0, 255, 0));
+      circle(img, mousePts.back(), 4, Scalar(0,0,255), -1, 8, 0);
+      circle(img, kfPts.back(), 4, Scalar(255,255,255), -1, 8, 0);
+      drawPath(img, mousePts, Scalar(255, 255, 0));
+      drawPath(img, kfPts, Scalar(0, 255, 0));
       imshow(windowName, img);
       code = (char)waitKey(100);
 
@@ -87,14 +85,6 @@ void onMouse(int event, int x, int y, int flags, void *param)
   mouseCoords.y = y;
 }
 
-void drawX(Mat &img, Point &center, Scalar color, int d)
-{
-  line(img, Point(center.x - d, center.y - d),
-       Point(center.x + d, center.y + d), color, 2, LINE_AA, 0);
-  line(img, Point(center.x + d, center.y - d),
-       Point(center.x - d, center.y + d), color, 2, LINE_AA, 0);
-}
-
 void drawPath(Mat &img, vector<Point> &points, Scalar color)
 {
   for (size_t i = 0; i < points.size() - 1; i++)
@@ -103,7 +93,7 @@ void drawPath(Mat &img, vector<Point> &points, Scalar color)
   }
 }
 
-void initKalmanFilter(KalmanFilter &KF,
+void initKalmanFilter(KalmanFilter &kf,
                       Point &xy,
                       Mat &transitionMatrix,
                       double procVariance,
@@ -111,27 +101,27 @@ void initKalmanFilter(KalmanFilter &KF,
                       double errorVariance)
 {
   // Initialize state vector (x, y, vx, vy)
-  KF.statePre.at<float>(0) = xy.x;
-  KF.statePre.at<float>(1) = xy.y;
-  KF.statePre.at<float>(2) = 0;
-  KF.statePre.at<float>(3) = 0;
+  kf.statePre.at<float>(0) = xy.x;
+  kf.statePre.at<float>(1) = xy.y;
+  kf.statePre.at<float>(2) = 0;
+  kf.statePre.at<float>(3) = 0;
 
-  KF.transitionMatrix = transitionMatrix;
+  kf.transitionMatrix = transitionMatrix;
 
   // Initialize measurement and covariance matrices (all scaled I matrices).
-  setIdentity(KF.measurementMatrix);
-  setIdentity(KF.processNoiseCov, Scalar::all(procVariance));
-  setIdentity(KF.measurementNoiseCov, Scalar::all(measVariance));
-  setIdentity(KF.errorCovPost, Scalar::all(errorVariance));
+  setIdentity(kf.measurementMatrix);
+  setIdentity(kf.processNoiseCov, Scalar::all(procVariance));
+  setIdentity(kf.measurementNoiseCov, Scalar::all(measVariance));
+  setIdentity(kf.errorCovPost, Scalar::all(errorVariance));
 
   if (false)
-    printKF(KF);
+    printKF(kf);
 }
 
-void printKF(KalmanFilter &KF)
+void printKF(KalmanFilter &kf)
 {
-  cout << "measurementMatrix = " << endl << " "  << KF.measurementMatrix << endl << endl;
-  cout << "processNoiseCov = " << endl << " "  << KF.processNoiseCov << endl << endl;
-  cout << "measurementNoiseCov = " << endl << " "  << KF.measurementNoiseCov << endl << endl;
-  cout << "errorCovPost = " << endl << " "  << KF.errorCovPost << endl << endl;
+  cout << "measurementMatrix = " << endl << " "  << kf.measurementMatrix << endl << endl;
+  cout << "processNoiseCov = " << endl << " "  << kf.processNoiseCov << endl << endl;
+  cout << "measurementNoiseCov = " << endl << " "  << kf.measurementNoiseCov << endl << endl;
+  cout << "errorCovPost = " << endl << " "  << kf.errorCovPost << endl << endl;
 }
